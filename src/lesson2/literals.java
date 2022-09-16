50803ba9bc50a2ef28e2b86fbcf1ef2432e371b2
VN <novictan15@gmail.com>
2022-09-16 07:57:34 +0300

    2 test commit

--- src/lesson2/literals.java
+++ src/lesson2/literals.java
@@ -2,6 +2,8 @@ package lesson2;
 
 public class literals {
     public static void main(String[] args) {
+
         System.out.println("Логический литерал "+true);
+        System.out.println("Логический литерал "+false);
     }
 }
